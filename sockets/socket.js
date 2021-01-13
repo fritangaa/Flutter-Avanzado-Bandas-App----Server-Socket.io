11d6e92636791fcd10ca26e4f80073df4559a45d
fritangaa Lopez <maximus.1626@gmail.com>
2021-01-13 00:00:20 -0600

    Cambios sobre app de bandas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,18 +1,46 @@
 const {io} = require('../index');
 
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+const bands = new Bands();
+
+bands.addBand(new Band("Heroes del silencio"));
+bands.addBand(new Band("Metallica"));
+bands.addBand(new Band("Bon Jovi"));
+
+//console.log(bands);
+
 // Mensajes de sockets 
 io.on('connection', client => {
 
     console.log("Cliente conectado");
+
+    client.emit("active-bands", bands.getBands() );
 
 
     client.on('disconnect', () => {
         console.log("Cliente desconectado");
     });
 
-    client.on("mensaje", ( payload ) => {
-        console.log("mensaje!!", payload);
 
-        io.emit("mensaje", {admin: "nuevo mensaje"});
+    client.on("vote-band", ( payload ) => {
+        //console.log("id de la banda: ", payload);
+        bands.voteBand(payload.id);
+        io.emit("active-bands", bands.getBands() );
     });
+
+    client.on("add-band", ( payload ) => {
+        //console.log("id de la banda: ", payload);
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit("active-bands", bands.getBands() );
+    });
+
+    client.on("delete-band", ( payload ) => {
+        //console.log("id de la banda: ", payload);
+        bands.deleteBand(payload.id);
+        io.emit("active-bands", bands.getBands() );
+    });
+
 });
+
